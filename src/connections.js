b3df7b9f9285a3eea3ec0a35c4779f82c2d1ed6e
Charles Morin <charlobino@hotmail.com>
2018-03-22 23:46:27 -0400

    Added connection functions
    addConnection, deleteConnection, modifyConnection, addDatabase

--- src/connections.js
+++ src/connections.js
@@ -1,61 +1,75 @@
 const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
 
-var title = 'gode-connections.json';
+const title = 'gode-connections.json';
 
-var createConfig = (hostname, port) => {
+var createDatabase = (address, port, dbName, collectName) => {
+    var url = `mongodb://${address}:${port}/${dbName}`;
+    MongoClient.connect(url, (err, client) => {
+        if (err) {
+            return console.log(`ERROR: ${dbName}`);
+        } else {
+            console.log(`SUCCESS: ${dbName}`);
+
+            const db = client.db(dbName);
+
+            db.collection('default').insertOne({}, (err, result) => {
+                if (err) {
+                    return console.log(`ERROR: ${collectName}`);
+                }
+            });
+        }
+        client.close();
+    });
+}
+
+var createConfig = (name, address, port) => {
     fs.appendFileSync(title, JSON.stringify({
-        connections: [{ id: 1, hostname, port }]
+        connections: [{ id: 1, name, address, port }]
     }));
 };
 
-var addConnection = (hostname, port) => {
+var addConnection = (name, address, port) => {
     if (fs.existsSync(title)) {
         try {
             var connObj = JSON.parse(fs.readFileSync(title));
             var lastId = connObj.connections[connObj.connections.length - 1].id;
             connObj.connections.push({
                 id: lastId + 1,
-                hostname: hostname,
+                name: name,
+                address: address,
                 port: port
             });
             fs.writeFileSync(title, JSON.stringify(connObj));
         } catch (err) {
-            createConfig(hostname, port);
+            createConfig(name, address, port);
         }
-    } else { createConfig(hostname, port); }
+    } else { createConfig(name, address, port); }
 };
 
 var deleteConnection = (id) => {
-    var connJson = JSON.parse(fs.readFileSync(title));
-    // ...
-    fs.writeFileSync(title, JSON.stringify(connJson));
+    var connObj = JSON.parse(fs.readFileSync(title));
+    connObj.connections.forEach(element => {
+        if (element.id === id) {
+            connObj.connections.splice(connObj.connections.indexOf(element), 1);
+        }
+    });
+    fs.writeFileSync(title, JSON.stringify(connObj));
 };
 
-addConnection('allo', '123123');
+var modifyConnection = (id, newName, newAddress, newPort) => {
+    var connObj = JSON.parse(fs.readFileSync(title));
+    connObj.connections.forEach(element => {
+        if (element.id === id) {
+            element.name = newName;
+            element.address = newAddress;
+            element.port = newPort;
+        }
+    });
+    fs.writeFileSync(title, JSON.stringify(connObj));
+};
 
-
-
-
-
-// var addConnection = (hostname, port) => {
-//     try {
-//         var connJson = JSON.parse(fs.readFileSync(title));
-//         var lastId = connJson.connections[connJson.connections.length - 1].id;
-//         connJson.connections.push({
-//             id: lastId + 1,
-//             hostname: hostname,
-//             port: port
-//         });
-//         fs.writeFileSync(title, JSON.stringify(connJson));
-//     } catch (err) {
-//         fs.appendFileSync(title, JSON.stringify({
-//             connections: [
-//                 {
-//                     id: 0,
-//                     hostname: hostname,
-//                     port: port
-//                 }
-//             ]
-//         }));
-//     }
-// };
+//modifyConnection(2, 'HomeConnection', '127.0.0.1','3000');
+//deleteConnection(1);
+//addConnection('LocalConnection','localhost', '27017');
+createDatabase('localhost', '27017', 'FirstDB', 'FirstCollection');
